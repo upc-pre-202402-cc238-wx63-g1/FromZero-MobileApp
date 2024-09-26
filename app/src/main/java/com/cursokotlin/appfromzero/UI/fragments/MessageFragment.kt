9744e596dae9e4001d2ee64ecfe6dac2150f0b84
JosephLlacchua <142326204+JosephLlacchua@users.noreply.github.com>
2024-09-25 22:37:00 -0500

    feat: add file picker functionality in MessageFragment

--- app/src/main/java/com/cursokotlin/appfromzero/UI/fragments/MessageFragment.kt
+++ app/src/main/java/com/cursokotlin/appfromzero/UI/fragments/MessageFragment.kt
@@ -1,9 +1,12 @@
 package com.cursokotlin.appfromzero.UI.fragments
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.cursokotlin.appfromzero.R
 
@@ -15,11 +18,36 @@ import com.cursokotlin.appfromzero.R
  */
 class MessageFragment : Fragment() {
 
+    private val PICK_FILE_REQUEST_CODE = 1
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_message, container, false)
+        val view = inflater.inflate(R.layout.fragment_message, container, false)
+
+        val attachFilesButton: Button = view.findViewById(R.id.attach_files)
+        attachFilesButton.setOnClickListener {
+            openFilePicker()
+        }
+
+        return view
+    }
+
+    private fun openFilePicker() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "*/*"
+        startActivityForResult(intent, PICK_FILE_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_FILE_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            data?.data?.let { uri ->
+
+                val filePath = uri.path
+
+            }
+        }
     }
 }
