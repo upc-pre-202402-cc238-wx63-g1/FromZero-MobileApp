92ac8647108b3f00e4a3e74d347019d02fa89765
ZadoR205 <zaidval@gmail.com>
2024-10-16 19:24:27 -0500

    feat: Created project model

---
+++ app/src/main/java/com/cursokotlin/appfromzero/models/Project.kt
@@ -0,0 +1,10 @@
+package com.cursokotlin.appfromzero.models
+
+class Project (
+    val title: String,
+    val description: String,
+    val technologies: String,
+    val frameworks: String,
+    val budget: String,
+    val processes: String
+    )
