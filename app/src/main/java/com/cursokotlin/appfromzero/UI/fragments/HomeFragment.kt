2c886f12d1e44edda4a000f1e084b01755c16b53
Jair Velasquez <106536136+Purak189@users.noreply.github.com>
2024-10-18 17:38:41 -0500

    feat: adding edit for profile card

--- app/src/main/java/com/cursokotlin/appfromzero/UI/fragments/HomeFragment.kt
+++ app/src/main/java/com/cursokotlin/appfromzero/UI/fragments/HomeFragment.kt
@@ -1,16 +1,22 @@
 package com.cursokotlin.appfromzero.UI.fragments
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.cursokotlin.appfromzero.R
 import com.cursokotlin.appfromzero.adapters.ProjectCardAdapter
 import com.cursokotlin.appfromzero.models.ProjectCard
+import org.w3c.dom.Text
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -29,12 +35,49 @@ class HomeFragment : Fragment() {
     private lateinit var adapter: ProjectCardAdapter
     private lateinit var projectList: List<ProjectCard>
 
+    // Home Edit Profile Components
+    private lateinit var ivEditProfile: ImageView
+    private lateinit var ivConfirmEditProfile: TextView
+
+    // Home Enterprise Profile Components
+    private lateinit var ivProfile: ImageView
+    private lateinit var tvEnterpriseWebsite: TextView
+    private lateinit var tvEnterpriseName: TextView
+    private lateinit var tvEnterpriseSector: TextView
+    private lateinit var tvEnterpriseRUC: TextView
+    private lateinit var tvEnterpriseDescription: TextView
+    private lateinit var tvEnterpriseCellphone: TextView
+
+    // Home Enterprise Extending Components layout
+    private lateinit var llExtending: LinearLayout
+
+
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        // Inicializar componentes
+        ivEditProfile = view.findViewById(R.id.ivEditProfile)
+        llExtending = view.findViewById(R.id.llExtending)
+
+        val flContainer: FrameLayout = view.findViewById(R.id.flContainer)
+
+        // Configurar el click listener
+        setUpClickListener()
+
+        // Configurar el touch listener para cerrar el modo de edición
+        flContainer.setOnTouchListener { _, _ ->
+            if (llExtending.visibility == View.VISIBLE) {
+                animateViewVisibility(llExtending, View.GONE)
+                ivEditProfile.visibility = View.VISIBLE
+                recyclerView.visibility = View.VISIBLE
+            }
+            false
+        }
 
         recyclerView = view.findViewById(R.id.rvProjects)
         recyclerView.layoutManager = LinearLayoutManager(context)
@@ -57,5 +100,40 @@ class HomeFragment : Fragment() {
         recyclerView.adapter = adapter
 
         return view
+    }
+
+    private fun setUpClickListener() {
+        ivEditProfile.setOnClickListener{
+            if (llExtending.visibility == View.GONE) {
+                recyclerView.visibility = View.GONE
+                ivEditProfile.visibility = View.GONE
+                animateViewVisibility(llExtending, View.VISIBLE)
+            } else {
+                llExtending.visibility = View.GONE
+                ivEditProfile.visibility = View.VISIBLE
+                recyclerView.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    private fun animateViewVisibility(view: View, visibility: Int, onAnimationEnd: (() -> Unit)? = null) {
+        if (visibility == View.VISIBLE) {
+            view.alpha = 0f
+            view.visibility = View.VISIBLE
+            view.animate()
+                .alpha(1f)
+                .setDuration(900)
+                .withEndAction {
+                    onAnimationEnd?.invoke()
+                }
+        } else {
+            view.animate()
+                .alpha(0f)
+                .setDuration(500)
+                .withEndAction {
+                    view.visibility = View.GONE
+                    onAnimationEnd?.invoke()
+                }
+        }
     }
 }
---
+++ app/src/main/java/com/cursokotlin/appfromzero/models/Enterprise.kt
@@ -0,0 +1,11 @@
+package com.cursokotlin.appfromzero.models
+
+class Enterprise (
+    val name: String,
+    val website: String,
+    val pictureUrl: String,
+    val description: String,
+    val field: String,
+    val socialRazon: String,
+    val cellphone: String
+)
