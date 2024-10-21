e6a24c2c10a35273eda3d1a11675d494a1de12ae
JosephLlacchua <142326204+JosephLlacchua@users.noreply.github.com>
2024-10-21 12:13:24 -0500

    feat: add SearchProjectFragment and class Project

--- app/src/main/java/com/cursokotlin/appfromzero/UI/fragments/SearchProjectFragment.kt
+++ app/src/main/java/com/cursokotlin/appfromzero/UI/fragments/SearchProjectFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.cursokotlin.appfromzero.R
-
+import com.cursokotlin.appfromzero.adapters.ProjectAdapter
+import com.cursokotlin.appfromzero.models.Project
 
 
 class SearchProjectFragment : Fragment() {
@@ -16,6 +19,19 @@ class SearchProjectFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_search_project, container, false)
+
+        val recyclerView :RecyclerView =view.findViewById(R.id.rvProjects)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+
+        val projects = listOf(
+            Project("Plataforma de Comercio Electrónico Geekit",
+                "La Plataforma de Comercio Electrónico Geekit es un proyecto destinado a crear una experiencia de compra en línea excepcional para nuestra marca de ropa y accesorios para jóvenes apasionados por la cultura geek. " +
+                        "La plataforma debe ofrecer una navegación intuitiva, una interfaz atractiva y funcionalidades que mejoren la experiencia " +
+                        "del usuario, desde la búsqueda de productos hasta el proceso de compra y seguimiento de pedidos"
+                , R.drawable.geekit_profile,"Geekit.pe")
+        )
+
+        recyclerView.adapter = ProjectAdapter(projects)
 
         return view
     }
---
+++ app/src/main/java/com/cursokotlin/appfromzero/models/Project.kt
@@ -0,0 +1,8 @@
+package com.cursokotlin.appfromzero.models
+
+data class Project (
+    val title: String,
+    val description: String,
+    val pictureLogo: Int,
+    val website :String
+)
