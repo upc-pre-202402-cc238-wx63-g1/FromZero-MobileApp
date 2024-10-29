b35a041dbee1f7268cc59eba5c9719ef6e931684
JosephLlacchua <142326204+JosephLlacchua@users.noreply.github.com>
2024-10-29 17:08:36 -0500

    feat : add configure_backend_connection, constants and update_resources

---
+++ app/src/main/java/com/cursokotlin/appfromzero/common/Constants.kt
@@ -0,0 +1,5 @@
+package com.cursokotlin.appfromzero.common
+
+object Constants {
+    const val BASE_URL = "http://10.0.2.2:8080/api/v1/"
+}
---
+++ app/src/main/java/com/cursokotlin/appfromzero/common/UIState.kt
@@ -0,0 +1,7 @@
+package com.cursokotlin.appfromzero.common
+
+data class UIState<T>(
+    val isLoading: Boolean = false,
+    val data: T? = null,
+    val message: String = ""
+)
