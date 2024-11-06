7542d9925836b11ccb34fb8b6160a95153ee96a0
JosephLlacchua <142326204+JosephLlacchua@users.noreply.github.com>
2024-11-06 17:00:37 -0500

    fix : view register developer and enterprise

--- app/src/main/java/com/cursokotlin/appfromzero/UI/fragments/RegisterFragment.kt
+++ app/src/main/java/com/cursokotlin/appfromzero/UI/fragments/RegisterFragment.kt
@@ -1,6 +1,7 @@
 package com.cursokotlin.appfromzero.UI.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +9,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.constraintlayout.widget.Guideline
 import androidx.fragment.app.Fragment
 import com.cursokotlin.appfromzero.R
 import com.cursokotlin.appfromzero.data.remote.RetrofitClient
@@ -35,16 +37,18 @@ class RegisterFragment : Fragment() {
         val etFirstNameLayout = rootView.findViewById<View>(R.id.etFirstNameLayout)
         val etLastNameLayout = rootView.findViewById<View>(R.id.etLastNameLayout)
         val etEnterpriseNameLayout = rootView.findViewById<View>(R.id.etEnterpriseNameLayout)
+        val guideline = rootView.findViewById<Guideline>(R.id.guideline) // Add this line
 
-        // Mostrar/ocultar campos según el rol del usuario
         if (userRole == "ROLE_DEVELOPER") {
             etFirstNameLayout.visibility = View.VISIBLE
             etLastNameLayout.visibility = View.VISIBLE
             etEnterpriseNameLayout.visibility = View.GONE
+            guideline.setGuidelineBegin(1520)
         } else if (userRole == "ROLE_ENTERPRISE") {
             etFirstNameLayout.visibility = View.GONE
             etLastNameLayout.visibility = View.GONE
             etEnterpriseNameLayout.visibility = View.VISIBLE
+            guideline.setGuidelineBegin(1310)
         }
 
         val btRegister = rootView.findViewById<Button>(R.id.bt_Registrar)
@@ -77,11 +81,14 @@ class RegisterFragment : Fragment() {
         return rootView
     }
 
-    private fun performDeveloperRegister(username: String, password: String, firstName: String, lastName: String) {
-        val request = DeveloperRegisterRequest(username, password, firstName, lastName)
+
+    private fun performDeveloperRegister(username: String, firstName: String, lastName: String, password: String) {
+        val request = DeveloperRegisterRequest(username, firstName, lastName, password)
+        Log.d("RegisterFragment", "DeveloperRegisterRequest: $request")
         authenticationRepository.registerDeveloper(request).enqueue(object : Callback<RegisterResponse> {
             override fun onResponse(call: Call<RegisterResponse>, response: Response<RegisterResponse>) {
                 if (response.isSuccessful) {
+                    Log.d("RegisterFragment", "Registration successful: ${response.body()}")
                     Toast.makeText(requireContext(), "Creación de cuenta exitosa", Toast.LENGTH_SHORT).show()
                     navigateToLoginFragment()
                 } else {
@@ -94,12 +101,13 @@ class RegisterFragment : Fragment() {
             }
         })
     }
-
-    private fun performEnterpriseRegister(username: String, password: String, enterpriseName: String) {
-        val request = EnterpriseRegisterRequest(username, password, enterpriseName)
+    private fun performEnterpriseRegister(username: String,enterpriseName: String,password: String) {
+        val request = EnterpriseRegisterRequest(username, enterpriseName,password)
+        Log.d("RegisterFragment", "DeveloperRegisterRequest: $request")
         authenticationRepository.registerEnterprise(request).enqueue(object : Callback<RegisterResponse> {
             override fun onResponse(call: Call<RegisterResponse>, response: Response<RegisterResponse>) {
                 if (response.isSuccessful) {
+                    Log.d("RegisterFragment", "Registration successful: ${response.body()}")
                     Toast.makeText(requireContext(), "Creación de cuenta exitosa", Toast.LENGTH_SHORT).show()
                     navigateToLoginFragment()
                 } else {
