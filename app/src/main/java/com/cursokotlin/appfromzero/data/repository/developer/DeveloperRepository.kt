16827597c201fe98d00c28402d30b822ee354926
anonimat0 <u202114714@upc.edu.pe>
2024-11-07 01:40:15 -0500

    feat: logig of conectio api of searchDeveloper added

--- app/src/main/java/com/cursokotlin/appfromzero/data/repository/developer/DeveloperRepository.kt
+++ app/src/main/java/com/cursokotlin/appfromzero/data/repository/developer/DeveloperRepository.kt
@@ -2,10 +2,15 @@ package com.cursokotlin.appfromzero.data.repository.developer
 
 import com.cursokotlin.appfromzero.data.remote.developer.DeveloperService
 import com.cursokotlin.appfromzero.models.profile.DeveloperProfileResponse
+import com.cursokotlin.appfromzero.models.profile.DeveloperSearchCard
 import retrofit2.Call
 
 class DeveloperRepository (private val developerService: DeveloperService) {
     fun getDeveloperByUserId(userId: Long, token: String): Call<DeveloperProfileResponse>{
         return developerService.getDeveloperByUserId(userId, "Bearer $token")
+    }
+
+    fun getDevelopers( token: String): Call <List<DeveloperSearchCard>>{
+        return developerService.getDevelopers("Bearer $token")
     }
 }
