7a07126d4ad3694981fa3944975dc3fd9af157d5
Samira-AA <samirajetzabel@gmail.com>
2024-11-21 12:24:14 -0500

    fix: search developer style

--- app/src/main/java/com/cursokotlin/appfromzero/common/Constants.kt
+++ app/src/main/java/com/cursokotlin/appfromzero/common/Constants.kt
@@ -2,6 +2,6 @@ package com.cursokotlin.appfromzero.common
 
 object Constants {
 
-    const val BASE_URL = "http://10.0.2.2:8080/api/v1/"
-    //const val BASE_URL = "https://fromzero-latest.onrender.com/api/v1/"
+    //const val BASE_URL = "http://10.0.2.2:8080/api/v1/"
+    const val BASE_URL = "https://fromzero-com.onrender.com/api/v1/"
 }
